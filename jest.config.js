e095b039861ea60acd419023111d2cf403b75c56
종원 <56539682+Bruce0203@users.noreply.github.com>
2022-06-23 14:56:00 +0000

    package lock

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,5 @@ module.exports = {
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
     testPathIgnorePatterns: [
-        '/node_modules/'
     ]
 };
